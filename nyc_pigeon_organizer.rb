205ac757fd1d10d59a11f0715dfc56ba84ca8c77
Hannah Drury <hannah_drury@yahoo.com>
2020-03-14 18:04:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,6 @@ def nyc_pigeon_organizer(data)
   # create a new hash
   # iterate through the hash until you get to the name array layer 
   # for each name, first check if it is a key in your new hash
-  # if it is not, then add it to the hash and assigns to it, an empty hash 
+  # if it is not, then add it to the hash and assigns to it, an empty hash
   
 end

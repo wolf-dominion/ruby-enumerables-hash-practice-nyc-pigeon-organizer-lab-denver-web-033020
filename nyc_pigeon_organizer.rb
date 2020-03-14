98bca269ae6216616668ae9abef97454dd62d535
Hannah Drury <hannah_drury@yahoo.com>
2020-03-14 16:39:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,19 @@ require 'pp'
 
 def nyc_pigeon_organizer(data)
 
+  hash.each do |key,value|
+  p key
+  if value.is_a?(Hash)
+    value.each do |k,v|
+      p k
+      p v
+    end
+  else
+    p value
+  end
+end
+
+
   
   pigeonNamesM = []
   pigeonNamesF = []

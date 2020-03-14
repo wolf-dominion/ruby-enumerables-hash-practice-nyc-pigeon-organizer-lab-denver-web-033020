b3bf51a7f04b93d16c8a1753772b1e4075522ac4
Hannah Drury <hannah_drury@yahoo.com>
2020-03-14 18:13:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,10 @@ require 'pp'
 def nyc_pigeon_organizer(data)
 
 data.each do |key, value| # loop through top most hash, retreives the keys and values
-  value.each do ||
-  
-  
+  value.each do |subkey, array|
+    
+    
+  end
 end
 
 

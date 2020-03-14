f92339b8536bb5daa708953388d6d4b87df175cb
Hannah Drury <hannah_drury@yahoo.com>
2020-03-14 18:16:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,8 +7,12 @@ def nyc_pigeon_organizer(data)
 
   data.each do |key, value| # loop through top most hash, retreives the keys and values
     value.each do |subkey, array| # loop through secondary hash, retreives the arrays
-      
-      
+      array.each do |name|
+        
+        # check if the name exists in pigeonList
+        
+        
+      end
     end
   end
 

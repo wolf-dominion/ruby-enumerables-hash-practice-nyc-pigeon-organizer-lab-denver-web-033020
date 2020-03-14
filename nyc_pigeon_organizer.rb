0150d17effb3c2ad94fc2c9ecb38fad8280953cd
Hannah Drury <hannah_drury@yahoo.com>
2020-03-14 18:44:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
+require 'pp'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  pigeonList = {}
+  
+
+  data.each do |key, value| # loop through top most hash, retreives the keys and values
+    value.each do |subkey, array| # loop through secondary hash, retreives the arrays
+      array.each do |name|
+        
+        # check if the name exists in pigeonList
+        if !pigeonList.key?(name)
+          pigeonList[name] = {}
+        end
+        
+        # check if the current main attribute(ex. color) is in the pigeonList's bird hash (the hash within a hash)
+        
+        if !pigeonList[name].key?(key)
+          pigeonList[name][key] = []
+        end
+        
+        pigeonList[name][key] << subkey.to_s
+        
+      end
+    end
+  end
+
+pigeonList
+  
 end

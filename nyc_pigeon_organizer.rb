9dfa96d6896d6b0e26f1eb2c4cf36f30457a5dde
Hannah Drury <hannah_drury@yahoo.com>
2020-03-14 16:58:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,11 @@ def nyc_pigeon_organizer(data)
   if value.is_a?(Hash)
     value.each do |k,v|
       v.each do |pigeonName|
-        newHash[pigeonName] = "hello"
+        if !newHash.key?(pigeonName)
+          newHash[pigeonName] = {}
+        else
+          newHash[pigeonName][key] = value
+        
       end
     end
   end
